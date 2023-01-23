0d27986fd048c928556c9da660ac116c65751e73
Alexandr Politov <razreztv@gmail.com>
2023-01-24 01:49:13 +0500

    add solution

--- task/Program.cs
+++ task/Program.cs
@@ -3,3 +3,29 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+Console.WriteLine("Введите элементы массива через пробел: ");
+string[] array = (Console.ReadLine() ?? "").Split();
+string[] arrResult = new string[array.Length];
+
+int len = 3;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= len)
+    {
+        arrResult[count] = array[i];
+        count++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arrResult);
